abbaee078930d3c25fd5024795f727f75be0da88
kacperwnuk <kacperwnuk97@o2.pl>
2019-03-21 20:40:59 +0100

    Dodano wstepną dokumentację.
    Stworzono część leksera.

--- Interpreter/lexer.py
+++ Interpreter/lexer.py
@@ -4,7 +4,7 @@ import copy
 
 
 class Lexer:
-    key_values = ['when', 'else', 'loop', 'object', 'fun', 'operator', 'main', 'int']
+    key_values = ['when', 'else', 'loop', 'object', 'fun', 'operator', 'main', 'int', 'in', 'out', 'extends', 'none']
 
     def __init__(self, source):
         self.source = source
@@ -19,17 +19,18 @@ class Lexer:
             symbol = last_char
         else:
             symbol = self.source.next_symbol()
-
         while self.is_white_character(symbol) or symbol == tokens.TokenType.t_comment.value:
             if symbol == 'EOF':
-                return tokens.Token(identifier=copy.copy(self.string), value=tokens.TokenType.t_eof)
+                line, column = self.source.line, self.source.column
+                return tokens.Token(identifier=copy.copy(self.string), value=tokens.TokenType.t_eof, line=line, column=column)
             if symbol == tokens.TokenType.t_comment.value:
                 self.escape_comment()
             symbol = self.source.next_symbol()
 
+        line, column = self.source.line, self.source.column
         self.string += symbol
         token_type = self.function_map[symbol]()
-        token = tokens.Token(identifier=copy.copy(self.string), value=token_type)
+        token = tokens.Token(identifier=copy.copy(self.string), value=token_type, line=line, column=column)
 
         return token
 
@@ -115,7 +116,98 @@ class Lexer:
         self.source.next_symbol()
         return tokens.TokenType.t_right_parenthesis
 
+    def check_if_assignment_or_comparison(self):
+        symbol = self.source.next_symbol()
+
+        if symbol == '=':
+            self.string += symbol
+            self.source.next_symbol()
+            return tokens.TokenType.t_comparison
+        else:
+            return tokens.TokenType.t_assignment
+
+    def check_if_logical_and(self):
+        symbol = self.source.next_symbol()
+
+        if symbol == '&':
+            self.string += symbol
+            self.source.next_symbol()
+            return tokens.TokenType.t_logical_and
+        else:
+            return tokens.TokenType.t_unknown
+
+    def check_if_logical_or(self):
+        symbol = self.source.next_symbol()
+
+        if symbol == '|':
+            self.string += symbol
+            self.source.next_symbol()
+            return tokens.TokenType.t_logical_or
+        else:
+            return tokens.TokenType.t_unknown
+
+    def check_if_negation_or_inequality(self):
+        symbol = self.source.next_symbol()
+
+        if symbol == '=':
+            self.string += symbol
+            self.source.next_symbol()
+            return tokens.TokenType.t_inequality
+        else:
+            return tokens.TokenType.t_negation_operator
+
+    def check_if_logical_or_output(self):
+        symbol = self.source.next_symbol()
+
+        if symbol == '=':
+            self.string += symbol
+            self.source.next_symbol()
+            return tokens.TokenType.t_loe
+        elif symbol == '<':
+            self.string += symbol
+            self.source.next_symbol()
+            return tokens.TokenType.t_output
+        else:
+            return tokens.TokenType.t_less
+
+    def check_if_logical_or_input(self):
+        symbol = self.source.next_symbol()
+
+        if symbol == '=':
+            self.string += symbol
+            self.source.next_symbol()
+            return tokens.TokenType.t_goe
+        elif symbol == '>':
+            self.string += symbol
+            self.source.next_symbol()
+            return tokens.TokenType.t_input
+        else:
+            return tokens.TokenType.t_greater
+
+    def addition_token(self):
+        self.source.next_symbol()
+        return tokens.TokenType.t_addition
+
+    def multiplication_token(self):
+        self.source.next_symbol()
+        return tokens.TokenType.t_multiplication
+
+    def division_token(self):
+        self.source.next_symbol()
+        return tokens.TokenType.t_division
+
+    def check_if_number_or_difference_token(self):
+        symbol = self.source.next_symbol()
+
+        if symbol in digits:
+            self.string += symbol
+            self.find_int()
+            return
+        else:
+            return tokens.TokenType.t_difference
+
     def create_dict(self):
+
         d = dict()
         d.update(d.fromkeys(ascii_letters, self.find_identifier))
         d.update(d.fromkeys(digits, self.find_int))
@@ -127,4 +219,16 @@ class Lexer:
         d['}'] = self.right_brace_token
         d['('] = self.left_parenthesis_token
         d[')'] = self.right_parenthesis_token
+        d['='] = self.check_if_assignment_or_comparison
+        d['&'] = self.check_if_logical_and
+        d['|'] = self.check_if_logical_or
+        d['!'] = self.check_if_negation_or_inequality
+        d['<'] = self.check_if_logical_or_output
+        d['>'] = self.check_if_logical_or_input
+        d['+'] = self.addition_token
+        d['*'] = self.multiplication_token
+        d['-'] = self.check_if_number_or_difference_token
+        d['/'] = self.division_token
+
         return d
+
